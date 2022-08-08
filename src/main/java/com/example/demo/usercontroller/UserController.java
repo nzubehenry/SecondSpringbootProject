db4a9738587bae076f0b2cb5a9dd251d49643dce
IgweNzubeHenry <nzubehenry06@gmail.com>
2022-08-08 12:04:47 +0100

    user home rest api completed

--- src/main/java/com/example/demo/usercontroller/UserController.java
+++ src/main/java/com/example/demo/usercontroller/UserController.java
@@ -1,9 +1,18 @@
 package com.example.demo.usercontroller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-	public UserController() {
-		// TODO Auto-generated constructor stub
+@RestController
+@RequestMapping("/user")
+
+public class UserController {
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		return "Welcome to USER HOME PAGE";
 	}
 
 }
