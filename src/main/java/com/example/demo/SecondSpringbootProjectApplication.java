0d272ba9a0dfe7e222f362f0ae44ed1ebbc96174
IgweNzubeHenry <nzubehenry06@gmail.com>
2022-08-08 11:26:45 +0100

    Initial Commit

---
+++ src/main/java/com/example/demo/SecondSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringbootProjectApplication.class, args);
+	}
+
+}
